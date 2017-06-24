ac20e4044fed3fee74bd0938bc4a1178a2881929
Tapi200 <takwada2000@yahoo.com>
2017-06-25 00:04:18 +1000

    Update Movies

--- Vidly/Models/Movie.cs
+++ Vidly/Models/Movie.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,7 +9,16 @@ namespace Vidly.Models
     public class Movie //plain old POCO
     {
         public int Id { get; set; }
+        [Required]
+        [StringLength(255)]
         public string Name { get; set; }
+        [Required]
+        public Genre Genre { get; set; } //navigation property
+        public byte GenreId { get; set; } // foreign key
+        public DateTime DateAdded { get; set; }
+        public DateTime ReleaseDate { get; set; }
+        public byte NumberInStock { get; set; }
+
     }
     
 }
